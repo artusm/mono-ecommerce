10275965b03801b11ba84aa3ac6fd2549e616dab
artusm <36174850+artusm@users.noreply.github.com>
2022-08-20 00:33:12 +0300

    test for <Button/>

---
+++ libs/shared/ui/src/props/constants.ts
@@ -0,0 +1,10 @@
+import { ButtonProps } from '../lib/Button';
+
+
+export const BUTTON_DEFAULT_PROPS: ButtonProps = {
+  children: 'hello',
+  tag: 'button',
+  size: 'base',
+  type: 'primary',
+  round: 'full',
+};
