ee636e4b9955b3f0ffd17fb38e886abbf49ae34e
artusm <36174850+artusm@users.noreply.github.com>
2022-08-21 18:31:51 +0300

    Add stories for <Icon/>

---
+++ libs/shared/ui/src/lib/Icon/Icon.stories.tsx
@@ -0,0 +1,13 @@
+import { ICON_DEFAULT_PROPS } from '../../props/constants';
+import { Story, Meta } from '@storybook/react';
+import { Icon, IconProps } from './Icon';
+
+export default {
+  component: Icon,
+  title: 'Components/Icon',
+} as Meta;
+
+const Template: Story<IconProps> = (args) => <Icon {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = ICON_DEFAULT_PROPS;
