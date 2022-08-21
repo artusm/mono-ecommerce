3504f17295091c40fe229fd506e893ec474d3c63
artusm <36174850+artusm@users.noreply.github.com>
2022-08-21 18:32:26 +0300

    Add stories for <Logo/>

---
+++ libs/shared/ui/src/lib/Logo/Logo.stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from '@storybook/react';
+import { Logo, LogoProps } from './Logo';
+
+export default {
+  component: Logo,
+  title: 'Components/Logo',
+} as Meta;
+
+const Template: Story<LogoProps> = (args) => <Logo {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
