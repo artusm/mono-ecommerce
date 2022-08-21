e5459dc3b319fc3286b9128d4a6fe6b4bf212d27
artusm <36174850+artusm@users.noreply.github.com>
2022-08-21 18:30:54 +0300

    Add stories for <CartSidebar/>

---
+++ libs/shared/ui/src/lib/CartSidebar/CartSidebar.stories.tsx
@@ -0,0 +1,27 @@
+import { Story, Meta } from '@storybook/react';
+import { CART_SIDEBAR_DEFAULT_PROPS } from '../../props/constants';
+import { CartSidebar, CartSidebarProps } from './CartSidebar';
+
+export default {
+  component: CartSidebar,
+  title: 'Components/Cart/Sidebar',
+} as Meta;
+
+const Template: Story<CartSidebarProps> = (args) => <CartSidebar {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  ...CART_SIDEBAR_DEFAULT_PROPS,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  ...CART_SIDEBAR_DEFAULT_PROPS,
+  isLoading: true,
+};
+
+export const NoUser = Template.bind({});
+NoUser.args = {
+  ...CART_SIDEBAR_DEFAULT_PROPS,
+  isUser: false,
+};
