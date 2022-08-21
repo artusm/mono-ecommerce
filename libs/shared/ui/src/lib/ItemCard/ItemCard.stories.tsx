ffb692f3abb9142dd4b1305ad3b4fb89d665e9b1
artusm <36174850+artusm@users.noreply.github.com>
2022-08-21 18:32:05 +0300

    Add stories for <ItemCard/>

---
+++ libs/shared/ui/src/lib/ItemCard/ItemCard.stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react';
+import { ITEM_CARD_DEFAULT_PROPS } from '../../props/constants';
+import { ItemCard, ItemCardProps } from './ItemCard';
+
+export default {
+  component: ItemCard,
+  title: 'Components/ItemCard',
+} as Meta;
+
+const Template: Story<ItemCardProps> = (args) => <ItemCard {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = ITEM_CARD_DEFAULT_PROPS;
