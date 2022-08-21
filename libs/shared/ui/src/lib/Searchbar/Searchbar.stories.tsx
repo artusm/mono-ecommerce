dd894c40e60b5c818286d26c3426e627d4425948
artusm <36174850+artusm@users.noreply.github.com>
2022-08-21 18:34:44 +0300

    Add stories for <Searchbar/>

---
+++ libs/shared/ui/src/lib/Searchbar/Searchbar.stories.tsx
@@ -0,0 +1,33 @@
+import { Story, Meta } from '@storybook/react';
+import { useState } from 'react';
+import { SEARCHBAR_ITEM } from '../../props/constants';
+import { Searchbar, SearchbarProps } from './Searchbar';
+
+export default {
+  component: Searchbar,
+  title: 'Components/Searchbar',
+} as Meta;
+
+const Template: Story<SearchbarProps> = (args) => {
+  const [input, setInput] = useState('');
+
+  return (
+    <div className="w-96">
+      <Searchbar
+        {...args}
+        input={input}
+        handleInput={(e) => setInput(e.currentTarget.value)}
+        handleSubmit={(e) => {
+          e.preventDefault();
+          console.log('submit');
+        }}
+        handleClear={() => setInput('')}
+      />
+    </div>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  items: [SEARCHBAR_ITEM],
+};
